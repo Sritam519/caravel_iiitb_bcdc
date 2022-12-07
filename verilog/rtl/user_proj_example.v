1cb5d14c646410be7f7a104a8ac28d0a6d7fed1e
Sritam519 <110079618+Sritam519@users.noreply.github.com>
2022-12-07 18:26:01 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -68,13 +68,16 @@ module user_proj_example #(
     // IRQ
     output [2:0] irq
 );
-    wire clk;
-    wire rst;
+    wire CK;
+    wire RN;
+    wire EN;
+    wire[7:0]Q;
+    wire[3:0]Qz,Qu;
 
     wire [`MPRJ_IO_PADS-1:0] io_in;
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
-
+/*
     wire [31:0] rdata; 
     wire [31:0] wdata;
     wire [BITS-1:0] count;
@@ -88,16 +91,21 @@ module user_proj_example #(
     assign wstrb = wbs_sel_i & {4{wbs_we_i}};
     assign wbs_dat_o = rdata;
     assign wdata = wbs_dat_i;
-
+*/
     // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+    assign io_out[35:28] = Q;
+    assign io_out[27:24] = Qz;
+    assign io_out[23:20] = Qu;
+    assign CK = wb_clk_i;
+    assign RN = wb_rst_i;
+    assign EN = io_in[`MPRJ_IO_PADS-9];
+    //assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
 
     // IRQ
     assign irq = 3'b000;	// Unused
 
     // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
+    /*assign la_data_out = {{(127-BITS){1'b0}}, count};
     // Assuming LA probes [63:32] are for controlling the count register  
     assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
     // Assuming LA probes [65:64] are for controlling the count clk & reset  
@@ -117,49 +125,42 @@ module user_proj_example #(
         .la_write(la_write),
         .la_input(la_data_in[63:32]),
         .count(count)
-    );
+    );*/
+    iiitb_bcdc dut(RN,EN,CK,Qz,Qu,Q);
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
-
+module iiitb_bcdc(RN,EN,CK,Qz,Qu,Q);
+	input RN,CK,EN;
+	output [3:0]Qz,Qu;
+	output [7:0]Q;
+	reg [3:0] Qz1,Qu1;
+	always@(posedge CK or negedge RN)
+		begin
+		if(!RN)
+			begin
+			Qz1=4'b0;
+			Qu1=4'b0;
+			end
+		else if(EN)
+			begin
+			if({Qz1,Qu1}==8'b10011001)
+				begin
+				Qz1=4'b0;
+				Qu1=4'b0;
+				end
+			else if(Qu1==4'b1001)
+				begin
+				Qu1=4'b0;
+				Qz1=Qz1+1;
+				if(Qz1==4'b1010)
+					Qz1=4'b0;
+				end
+			else Qu1=Qu1+1;
+			end
+		end
+	assign Q={Qz1,Qu1};
+	assign Qu=Qu1;
+	assign Qz=Qz1;
 endmodule
 `default_nettype wire
